d426c788f6e56fd4bccdae47c1c1e269547895f6
yedidimr <yedidimr@gmail.com>
2015-12-08 13:45:30 +0200

    refactor python script complete

--- modules/refactor.py
+++ modules/refactor.py
@@ -1,128 +1,60 @@
+import os
 import logging
 from sklearn import preprocessing
 from sklearn.decomposition import PCA
-from numpy import dot, linalg, sqrt,apply_along_axis
+from numpy import dot, linalg, sqrt,hstack
 
 VERSION = 1.0 #TODO move to other place
 
 def refactor(O, K, t, num_components, ranked_filename='refactor.out.rankedlist.txt', components_filename='refactor.out.components.txt'):
 
-    print('Starting ReFACTor v%s...' % VERSION);
-    # logging.info('Starting ReFACTor v%s...' % VERSION);
-
-    print len(O)
-    print len(O[0])    
-    # print O[0] # in R print(O[1,])
-    # print O.transpose()
-    # sample_id <- O[1, -1] # extract samples ID
-    # O <- O[-1,] # remove sample ID from matrix
-    # cpgnames <- O[, 1] ## set rownames
-    # O <- O[, -1] 
-    # O = matrix(as.numeric(O),nrow=nrow(O),ncol=ncol(O))
-
-    # num_components = K;
-
-    print('Running a standard PCA...')
-    # logging.info('Running a standard PCA...')
-    scaledO = preprocessing.StandardScaler().fit(O.transpose()).transform(O.transpose())
-
-    # print scaler.transform(O.transpose())
-    # print(dir(scaler))
-    # import pdb
-    # pdb.set_trace()
-    pca = PCA(n_components=num_components).fit(scaledO)
-    # pcs = pca.fit(O.transpose())
-    # pcs = prcomp(scale( t(O) ));
-
-
-
-    coeff = pca.components_
-    print "coeff"
-
-    print len(coeff)
-    print len(coeff[0])
-    print coeff[0]
-    print "_____________________0"
-    # coeff = pcs$rotation #U   pcs.components_
-    score = pca.transform( scaledO ) # TODO or score = pca.transform(O.transpose())
-    print "score"
-    print len(score)
-    print len(score[0])
-    print score[0]
-    # score = pcs$x #P pcs.transform(X)
-    # print score
-
-    print "_____________________1"
-    logging.info('Compute a low rank approximation of input data and rank sites...')
-    x = dot(score[:,1:K], coeff[:,1:K].transpose())
-    print len(x)
-    print len(x[0])
-    print x[0]
-    # x = score[,1:K]%*%t(coeff[,1:K]);
-    # print x
-    print "_____________________2"
-    An = preprocessing.StandardScaler( with_mean = True, with_std = False ).fit( O.transpose() ).transform(O.transpose())
-    # An = scale(t(O),center=T,scale=F)
-    # print An.transform(O.transpose())
+    logging.info('Starting ReFACTor v%s...' % VERSION);
+  
+    sample_ids = O[0,:][1:] # extract samples ID
+    cpgnames = O[:,0][1:]   # extract methylation sites names
+    O = O[1:,1:].astype(float) # remove sample ID and cpgnames from matrix
     
-    print "_____________________3"
+    logging.info('Running a standard PCA...')
+    scaledO = preprocessing.StandardScaler().fit(O.transpose()).transform(O.transpose())
+    pca = PCA().fit(scaledO) 
+
+    coeff = pca.components_.transpose()
+    score = pca.transform( scaledO )
+    
+    logging.info('Compute a low rank approximation of input data and rank sites...')
+    x = dot(score[:,0:K] , coeff[:,0:K].transpose())
+    An = preprocessing.StandardScaler( with_mean = True, with_std = False ).fit( O.transpose() ).transform(O.transpose())
     Bn = preprocessing.StandardScaler( with_mean = True, with_std = False ).fit( x ).transform(x)
-    print len(Bn)
-    print len(Bn[0])
-    print Bn[0]
-    # Bn = scale(x,center=T,scale=F)
-    # print Bn.transform(x)
+    An = (An * 1 / sqrt((An**2).sum(axis=0)))
+    Bn = ( Bn * (1 / sqrt((Bn**2).sum(axis=0))))
 
-    print "_____________________4"
+    # Find the distance of each site from its low rank approximation.
+    distances = sqrt(((An - Bn)**2).sum(axis=0))#**0.5
+    ranked_list = distances.argsort()
+    distances.sort() 
 
-
-    An = (An * 1 / sqrt((An**2).sum(axis=0))).transpose() 
-    # An = t(t(An)*(1/sqrt(apply(An^2,2,sum))))
- 
-    print "_____________________5"
-    Bn = ( Bn * (1 / sqrt((Bn**2).sum(axis=0)))).transpose() 
-    # Bn = t(t(Bn)*(1/sqrt(apply(Bn^2,2,sum))))
-    print len(Bn)
-    print len(Bn[0])
-    print Bn[0]
-    print "_____________________6"
-
-    # # Find the distance of each site from its low rank approximation.
-    distances = apply_along_axis(sum, 1, (An - Bn)**2)
-    # distances = apply((An-Bn)^2,2,sum)^0.5 ;
-    # print distances
-    print "_____________________7"
-    dsort = sort(distances) #???
-    # dsort = sort(distances,index.return=T);
-    # ??? https://stat.ethz.ch/R-manual/R-devel/library/base/html/sort.html
-    ranked_list = dsort #???
-    # ranked_list = dsort$ix
-
-    print('Compute ReFACTor components...')
-    # logging.info('Compute ReFACTor components...')
-    sites = ranked_list[1:t]
-    # sites = ranked_list[1:t];
+    logging.info('Compute ReFACTor components...')
+    sites = ranked_list[0:t]
     scaledOsites = preprocessing.StandardScaler().fit(O[sites,:].transpose()).transform(O[sites,:].transpose())
-    pca = PCA(n_components=num_components).fit(scaledOsites)
-    # pcs = prcomp(scale(t(O[sites,])));
-    first_score = score[:,1:K]
-    # first_score <- score[,1:K];
-    score = pca.transform(scaledOsites) # score = pca.transform(O[sites,:].transpose())
-    # score = pcs$x
+    pca = PCA().fit(scaledOsites)
+    first_score = score[:,0:K]
+    score = pca.transform(scaledOsites)
 
     logging.info('Saving a ranked list of the data features...');
-    file(ranked_filename, 'wb').write( ranked_list ) #one line seperate with , ??? 
-    # write(t(cbind(ranked_list,cpgnames[ranked_list])),file=ranked_filename,ncol=2)
+
+    if  os.path.exists(ranked_filename):
+        os.remove(ranked_filename) #TODO  dont remove.. do other thing..!
+    with open(ranked_filename, "a") as f:
+        for index in ranked_list:
+            f.write("%s %s\n"% (index, cpgnames[index]))
 
     logging.info('Saving the ReFACTor components...');
-    s=""
-    for i in len(score):
-        s += score[i][1:num_components]
-    file(components_filename, 'wb').write( s) #???
-    # write(t(score[,1:num_components]),file=components_filename,ncol=num_components)
 
-    return s, ranked_list, first_score #score[,1:num_components], ranked_list, first_score 
-    # result <- list(refactor_components=score[,1:num_components],ranked_list=ranked_list, first_pca = first_score) 
-    # return(result)
-
+    if  os.path.exists(components_filename):
+        os.remove(components_filename) #TODO dont remove.. do other thing..!
+    with open(components_filename, "a") as f:
+        for line in score[:,0:K]:
+            f.write("\t".join([str(i) for i in line]) + "\n")
+    
+    return score[:,0:num_components], ranked_list, first_score 
 
