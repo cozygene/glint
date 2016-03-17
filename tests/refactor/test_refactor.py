86bf409319a9f20305a198ef143d54f92362a538
yedidimr <yedidimr@gmail.com>
2016-03-17 19:41:52 +0200

    Change data file so number of sites will be different than number of samples. Add test for phenotype fs

--- tests/refactor/test_refactor.py
+++ tests/refactor/test_refactor.py
@@ -1,5 +1,6 @@
 from modules import refactor,methylation_data
-from numpy import loadtxt, array_equal
+from numpy import loadtxt, array_equal, corrcoef
+from utils import LinearRegression
 import logging
 
 class FeatureSelectionTester():
@@ -8,13 +9,15 @@ class FeatureSelectionTester():
     def __init__(self):
         self.fs_meth_data = methylation_data.MethylationData(datafile = self.FAKE_DATA)
         self.test_controls_fs()
-        logging.info("PASS")
+        self.test_phenotype_fs()
+        # logging.info("PASS")
 
     def test_controls_fs(self):
         """
         test that all samples that are '1' in the FAKE_CONTROL file are deleted from FAKE_DATA file
 
         """
+        logging.info("Testing controls feature selection...")
         refactor_meth_data = self.fs_meth_data.copy()
 
         controls = loadtxt(self.FAKE_CONTROL, dtype = str)
@@ -23,14 +26,14 @@ class FeatureSelectionTester():
                       k = 2, 
                       feature_selection = "controls",
                       phenofile = self.FAKE_CONTROL,
-                      t=7)
+                      t=5)
         index =0
         res = module.feature_selection_handler()
 
         # res is the data matrix found in FAKE_DATA without the columns (samples) at index i when i is index of a 0 in FAKE_CONTROL file
         for i in range(len(controls_indices)):
             if controls_indices[i]=='0':
-                assert array_equal([float(i + x*10) for x in range(10)] ,res[:,index])
+                assert array_equal([float(i + x*10) for x in range(6)] ,res[:,index])
                 index+=1
 
         # controls fs result will contain  number of columns (samples) as the number of 0's in the FAKE_CONTROL file
@@ -38,5 +41,32 @@ class FeatureSelectionTester():
 
         # controls fs suppose to change methylation data
         assert array_equal(module.meth_data.data, res)
+        logging.info("PASS")
 
+    def test_phenotype_fs(self):
+        logging.info("Testing phenotype feature selection...")
+        refactor_meth_data = self.fs_meth_data.copy()
+
+        phenotype = loadtxt(self.FAKE_CONTROL, dtype = str)[:,1].astype(float)
+        print phenotype
+
+        module  = refactor.Refactor(methylation_data = refactor_meth_data, 
+                      k = 2, 
+                      feature_selection = "phenotype",
+                      phenofile = self.FAKE_CONTROL,
+                      t=5)
+
+        # validate phenotype feature selection output (res_data) is correlated to our linear regression for (site, phenotype)
+        res_data = module.feature_selection_handler()
+        for i,site in enumerate(refactor_meth_data.data):
+            lin_reg = LinearRegression(site, phenotype)
+            assert len(lin_reg.residuals) == len(site)
+            cor = corrcoef(lin_reg.residuals, res_data[i])
+
+            # validate our residuals are corelated to res_data
+            assert abs( 1- cor[0][1]) < 1e-4 
+
+        logging.info("PASS")
+
+    
         
