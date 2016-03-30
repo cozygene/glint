608a342809ce1bb81f9eb785e309cfb785524c75
yedidimr <yedidimr@gmail.com>
2016-03-30 15:07:01 +0300

    Change lin reg test files

--- tests/utils/test_lin_reg.py
+++ tests/utils/test_lin_reg.py
@@ -1,25 +1,29 @@
 
-from numpy import loadtxt, corrcoef
+from numpy import loadtxt, corrcoef, empty_like
 from utils import tools, pca, LinearRegression, common
 import logging
 
 class LinearRegressionTester():
-    LIN_REG_DATA = "tests/ewas/files/lin_reg_test_data.txt"
-    LIN_REG_PHENO = "tests/ewas/files/lin_reg_test_pheno.txt"
-    LIN_REG_RESIDUALS = "tests/ewas/files/lin_reg_test_res.txt"
+    LIN_REG_X= "tests/utils/files/lin_reg_test_x.txt"
+    LIN_REG_Y = "tests/utils/files/lin_reg_test_y.txt"
+    LIN_REG_RESIDUALS = "tests/utils/files/lin_reg_test_res.txt"
 
     def __init__(self):
-        data = loadtxt(self.LIN_REG_DATA)
-        pheno = loadtxt(self.LIN_REG_PHENO)
+        """
+        check linear regression (y-x)
+        """
+        y = loadtxt(self.LIN_REG_Y)
+        x = loadtxt(self.LIN_REG_X)
         residuals = loadtxt(self.LIN_REG_RESIDUALS)
 
-        lin_reg = LinearRegression(pheno, data)
+         
+        if len(y.shape) == 1:
+            lin_reg = LinearRegression(y, x)
+            cor = corrcoef(lin_reg.residuals, residuals)
+            assert abs( 1- cor[0][1]) < 1e-4 
+            assert len(lin_reg.residuals) == len(x)
 
-        assert len(lin_reg.residuals) == len(pheno)
-
-        cor = corrcoef(lin_reg.residuals, residuals)
-
-        # validate our residuals are corelated to LIN_REG_RESIDUALS
-        assert abs( 1- cor[0][1]) < 1e-4 
+        elif len(y.shape) == 2:  
+            raise("TEST WASNT IMPLEMENTED")
 
         logging.info("PASS")
