b303d5a9b1d50ad2363f3388b3099be1b081d8a1
yedidimr <yedidimr@gmail.com>
2016-07-26 10:56:51 +0300

    add test for predictor

--- tests/predictor/test_predictor.py
+++ tests/predictor/test_predictor.py
@@ -9,11 +9,12 @@ class PredictorTester():
     GENO_FILE = "tests/predictor/files/reut22_10samples_5snps.geno"
     MISSING_SAMPLES_GENO_FILE = "tests/predictor/files/reut22_10samples_5snps-missing_samples.geno"
     MISSING_SNPS_GENO_FILE = "tests/predictor/files/reut22_10samples_5snps-missing_snps.geno"
+    MISSING_SAMPLES_AND_SNPS_GENO_FILE = "tests/predictor/files/reut22_10samples_5snps-missing_samples_and_snps.geno"
     IND_FILE = "tests/predictor/files/reut22_10s.ind"
     MANUALLY_PREDICTED_FILE = "tests/predictor/files/reut22_test_out" 
     MISSING_SAMPLES_MANUALLY_PREDICTED_FILE = "tests/predictor/files/reut22_test_out-missing_samples" 
     MISSING_SNPS_MANUALLY_PREDICTED_FILE = "tests/predictor/files/reut22_test_out-missing_snps" 
-
+    MISSING_SAMPLES_AND_SNPS_MANUALLY_PREDICTED_FILE = "tests/predictor/files/reut22_test_out-missing_samples_and_snps"
     SITES_SCORES_FILE = 'parsers/assets/sites_scores_list'
     SITES_SNPS_FILE = 'parsers/assets/site_snps_list'
     SITES_IDS_FILE ='parsers/assets/sites_ids_list'
@@ -28,6 +29,7 @@ class PredictorTester():
         self.test_predict()
         self.test_predict_missing_samples()
         self.test_predict_missing_snps()
+        self.test_predict_missing_samples_and_snps()
         logging.info("Testing Finished on PredictorTester")
 
     def test_predict(self):
@@ -74,3 +76,19 @@ class PredictorTester():
 
         logging.info("PASS")
 
+
+
+    def test_predict_missing_samples_and_snps(self):
+        logging.info("Testing test_predict_missing_samples_and_snps...")
+        predicted = loadtxt(self.MISSING_SAMPLES_AND_SNPS_MANUALLY_PREDICTED_FILE)
+        self.module.predict(self.MIN_SCORE, self.SNPS_FILE, self.MISSING_SAMPLES_AND_SNPS_GENO_FILE, self.IND_FILE, self.MIN_MISSING_VALUES)
+        
+        assert(predicted.shape[0] == self.module.site_prediction.shape[0])
+        assert(predicted.shape[1] == self.module.site_prediction.shape[1])
+
+        for i in range(predicted.shape[1]):
+            assert(tools.correlation(predicted[:,i], self.module.site_prediction[:,i]))
+        
+        for i in range(predicted.shape[0]):
+            assert(tools.correlation(predicted[i,:], self.module.site_prediction[i,:]))
+        logging.info("PASS")
