dfdd39eb1974b3b6fba68d7815211f2b7670fd20
yedidimr <yedidimr@gmail.com>
2016-09-22 18:01:34 +0300

    fix  tests

--- main_test.py
+++ main_test.py
@@ -5,12 +5,11 @@ from tests.refactor import test_refactor
 from tests.ewas import test_ewas
 from tests.utils import test_lin_reg, test_pca, test_tools
 from tests.methylation_data import test_methylation_data
-from tests.kit import test_kit
 from tests.predictor import test_predictor
 from tests.missing_values import replace_missing_values_test
 
 test_refactor.RefactorTester()
-test_refactor.FeatureSelectionTester()
+test_refactor.SenariosTester()
 test_lin_reg.LinearRegressionTester()
 test_lin_reg.LogisticRegressionTester()
 test_methylation_data.DataTester()
