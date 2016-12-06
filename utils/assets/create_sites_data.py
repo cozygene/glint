19100b99d81297cfa937eb37a46eb7dcd15ab14c
yedidimr <yedidimr@gmail.com>
2016-12-06 18:08:30 +0200

    add script to create cpg info file

--- utils/assets/create_sites_data.py
+++ utils/assets/create_sites_data.py
@@ -1,6 +1,19 @@
+import os
 from numpy import where, vstack, savetxt
-from pandas import Index, unique
-from pandas import read_csv, DataFrame
+from pandas import Index, unique, read_csv, DataFrame
+
+"""
+this script creates the HumanMethylationSites file (you can choose output file name with outputfile parameter)
+it extracts the fields from the chip data
+it first reads the data from 850K than only adds information about new cpg sites from the rest of the files
+(it takes all the sites from file files[0] than adds the different sites between files[1] and files[0],
+    than adds only the different sites between files[2] and files[1]....)
+
+The first file in the files list is assumed to be the most updated one (the one to first take the information from)
+
+to add new chip, add another filepath (like filepath_850) and add it to the list of files ("files" parameter) in the right location 
+where the first file (location 0) is considered the most updated data file
+"""
 
 t=['IlmnID','CHR','MAPINFO','UCSC_RefGene_Name','Relation_to_UCSC_CpG_Island'] # the interesting fields
 repeat_field = 'UCSC_RefGene_Name'
@@ -9,6 +22,7 @@ repeat_field = 'UCSC_RefGene_Name'
 filepath_850 = '/Users/yedidimr/Downloads/MethylationEPIC_v-1-0_B2.csv'
 filepath_450 = '/Users/yedidimr/Downloads/HumanMethylation450_15017482_v1-2.csv'
 filepath_270 = '/Users/yedidimr/Downloads/humanmethylation27_270596_v1-2.bpm'
+files = [filepath_850, filepath_450]
 
 # path to the output file
 outputfile = "HumanMethylationSites_new"
@@ -31,22 +45,37 @@ def get_data(filepath):
     data = data.astype(str)
     return data
 
+def new_cpgs(old_data, new_data):
+    """
+    cpg are assumed to be in the first column
+    return the indexes of the cpg ids in the new_data that are not found in the old_data
+    """
+    old_cpgid =  old_data[:,0]
+    new_cpgid =  new_data[:,0]
 
-print "loading %s "% filepath_850
-data850 = get_data(filepath_850)
-cpgid850 =  data850[:,0]
+    # find indexes cpg sites in the new data which do not exist in the old data
+    indexes_list = where(Index(unique(old_cpgid)).get_indexer(new_cpgid) <0)[0]
+    return indexes_list
 
-print "loading %s "% filepath_450
-data450 = get_data(filepath_450)
-cpgid450 =  data450[:,0]
 
-# find indexes cpg sites in 450K which do not exist in 850K
-indexes_list = where(Index(unique(cpgid850)).get_indexer(cpgid450) <0)[0]
-print "found %s cpgs in 450K which do not exist in 850K" % len(indexes_list) 
-if len(indexes_list) != 0: 
-    data = vstack((data850, data450[indexes_list,:]))
-else:
-    data = data850
+"""
+read all the files
+"""
+data = None
+for i,datafile in enumerate(files):
+    print "loading %s "% datafile
+
+    if data is None:
+        data = get_data(datafile)
+    else:
+        new_data = get_data(datafile)
+        
+        # find indexes cpg sites in 450K which do not exist in 850K
+        new_cpg_indexes =  new_cpgs(data, new_data)
+        if len(new_cpg_indexes) > 0:
+            print "found %s cpgs in %s which do not exist in the more updated datafiles (those who were previous loaded)" % (len(new_cpg_indexes) , os.path.basename(datafile))
+            data = vstack((data, new_data[new_cpg_indexes,:]))
+            
 
 """
 remove repeats in the UCSC field:
